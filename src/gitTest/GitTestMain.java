01f5549f52d68f9174da87e7e5d29d7a3f096a64
takkasila <takkasila@outlook.com>
2014-09-01 11:51:40 +0700

    Show more lines

--- src/gitTest/GitTestMain.java
+++ src/gitTest/GitTestMain.java
@@ -3,7 +3,8 @@ package gitTest;
 public class GitTestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, git");
+		System.out.println("Hello");
+		System.out.println("Welcome to git");
 
 	}
 
