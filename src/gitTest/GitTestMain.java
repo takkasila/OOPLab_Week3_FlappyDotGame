c113dec9adce3cef70fc2efe25897050e6e4739f
takkasila <takkasila@outlook.com>
2014-09-01 11:54:22 +0700

    Empty Dog class.

--- src/gitTest/GitTestMain.java
+++ src/gitTest/GitTestMain.java
@@ -3,8 +3,12 @@ package gitTest;
 public class GitTestMain {
 
 	public static void main(String[] args) {
+		Dog d = new Dog();
+		
 		System.out.println("Hello");
 		System.out.println("Welcome to git");
+		
+		
 
 	}
 
