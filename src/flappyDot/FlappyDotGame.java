c6d729f137808b6a01ecb722ce93a1452c147d84
takkasila <takkasila@outlook.com>
2014-09-01 12:30:33 +0700

    Import Slick2D Extends from BasicGame

--- src/flappyDot/FlappyDotGame.java
+++ src/flappyDot/FlappyDotGame.java
@@ -1,10 +1,39 @@
 package flappyDot;
 
-public class FlappyDotGame {
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class FlappyDotGame  extends BasicGame{
+	
+
+	public FlappyDotGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
