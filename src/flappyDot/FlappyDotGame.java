7ffdabfd601af4f2f99da4275561a30f609e0d71
takkasila <takkasila@outlook.com>
2014-09-01 12:38:56 +0700

    Create game Change background color

--- src/flappyDot/FlappyDotGame.java
+++ src/flappyDot/FlappyDotGame.java
@@ -1,6 +1,9 @@
 package flappyDot;
 
+
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -14,22 +17,31 @@ public class FlappyDotGame  extends BasicGame{
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FlappyDotGame game = new FlappyDotGame("Flappy Dot");
+		AppGameContainer appgc;
+		try {
+			appgc = new AppGameContainer(game);
+			appgc.setDisplayMode(800, 600, false);
+			appgc.setTargetFrameRate(60);
+			appgc.start();
+		} catch (SlickException e) {
+			
+			e.printStackTrace();
+		}
 
 	}
 
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		Color background = new Color(128, 128, 128);
+		arg0.getGraphics().setBackground(background);
+		
+	}
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void init(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
 		// TODO Auto-generated method stub
